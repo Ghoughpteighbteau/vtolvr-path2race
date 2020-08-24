4209ccf9b2494d8c812a5e818695594661e298e6
ghoughpteighbteau <lierorules@gmail.com>
2020-08-23 19:37:54 -0700

    imported vtsparser

--- src.ts
+++ src.ts
@@ -1,3 +1,5 @@
+import * as vtsparser from './vtsparser';
+
 const invts_elem = document.getElementById("invts") as HTMLInputElement;
 invts_elem.addEventListener('change', () =>{
   invts_elem.files[0].text()
